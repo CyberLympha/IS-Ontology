71e6fb76553541b6a250e385dca58a2dc28fffc7
upaev <upaevkirill@gmail.com>
2025-04-04 19:46:09 +0500

    в C:\Users\upaev\Desktop\УрФУ\ВКР\IS-Ontology\IS_ontology\IS_ontology\Clf\models.py была создана модель для хранения информации об источнике (статье, веб-странице и т.п.) в виде класса Source, которая ранее была неопределена

--- IS_ontology/IS_ontology/Clf/models.py
+++ IS_ontology/IS_ontology/Clf/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django.db import models # type: ignore
 
 # Create your models here.
+
+class Source(models.Model):
+    """
+    Модель для хранения информации об источнике (статье, веб-странице и т.п.).
+
+    Атрибуты:
+        url (URLField): Уникальный адрес источника.
+        description (TextField): Краткое описание содержания источника.
+        date (DateField): Дата добавления источника (устанавливается автоматически при создании).
+    """
+    url = models.URLField(unique=True, help_text="Уникальный URL источника")
+    description = models.TextField(help_text="Описание статьи или веб-страницы")
+    date = models.DateField(auto_now_add=True, help_text="Дата добавления записи")
+
+    def __str__(self):
+        """
+        Возвращает строковое представление объекта Source.
+
+        В данном случае — URL источника, что удобно для отображения в админке,
+        логах и отладке.
+        """
+        return self.url
