5455eee2573cba18d5eabc9883faa6ea5df6deca
upaev <upaevkirill@gmail.com>
2025-04-15 16:22:43 +0500

    feat(routing): подключены маршруты для API и системы аккаунтов
    - Добавлен путь 'accounts/', подключающий маршруты из IS_ontology.Accounts.urls
    - Добавлен путь 'api/', подключающий маршруты из IS_ontology.api.urls
    - Вынес api-путь в отдельную переменную 'api_patterns' для лучшей читаемости и возможного расширения
    - Структуризация файла urls.py в соответствии с рекомендациями Django

--- IS_ontology/IS_ontology/Clf/urls.py
+++ IS_ontology/IS_ontology/Clf/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path
+from django.contrib import admin 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, re_path, include
+from rest_framework.schemas import get_schema_view
 
-from . import views
-
-
-app_name = 'Clf'
-
+api_patterns = [
+    path("api/", include("IS_ontology.api.urls")),
+]
 
 urlpatterns = [
-    path('', views.index, name='clf'),
+    path("admin/", admin.site.urls), 
+    path("accounts/", include("IS_ontology.Accounts.urls")),
+    api_patterns[0],
+    ...
 ]
