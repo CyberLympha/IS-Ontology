aa55659808a7b9cae2f79662537b7db55802a088
upaev <upaevkirill@gmail.com>
2025-04-26 20:02:52 +0500

    Коммит: debug/find-outdated-code-in-container Изменения: Проведена проверка содержимого файлов в контейнере через docker compose exec web cat ....
    Цель:
    Убедиться, что монтируемая папка IS_ontology действительно содержит актуальный код проекта.
    
    Вывод:
    Внутри контейнера /apps/IS_ontology/IS_ontology/Ie/urls.py обнаружен старый код (AddView вместо test_add_view),
    что указывает на проблему двойной вложенности каталогов (IS_ontology/IS_ontology/) в локальной структуре проекта.

--- IS_ontology/IS_ontology/Ie/urls.py
+++ IS_ontology/IS_ontology/Ie/urls.py
@@ -2,13 +2,20 @@ from django.urls import path
 
 from . import views
 
+print("Мой Ie/urls.py загружен") #отладка
 
 app_name = 'Ie'
+
+from django.http import HttpResponse
+
+def test_add_view(request):
+    return HttpResponse("✅ Работает AddView") #отладка
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='ie'),
     path('graph', views.graph, name='graph'),
-    path('add', views.AddView.as_view(), name='add'),
+    # path('add', views.AddView.as_view(), name='add'),
+    path('add', test_add_view), #отладка
     path('predicates', views.PredicateView.as_view(), name='predicates'),
     path('build_graph', views.build_graph)
 ]
